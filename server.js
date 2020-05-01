a952b80362458fc97b0cd1c30316e7f88e13dd2b
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-04-30 18:23:22 -0600

    Add array of messages

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ const http = require('http');
 const url  = require('url');
 const server = http.createServer();
 
+let messages = [
+    { 'id': 1, 'user': 'brittany storoz', 'message': 'hi there!' },
+    { 'id': 2, 'user': 'bob loblaw', 'message': 'check out my law blog' },
+    { 'id': 3, 'user': 'lorem ipsum', 'message': 'dolor set amet' }
+]
+
 server.listen(3000, () => {
-    console.log('The HTTP server is listening on Port 3000.');
-    
+    console.log('The HTTP server is listening on Port 3000.');     
 })
+
+ 
 
 server.on('request', (request, response) => {
     response.writeHead(200, { 'Content-Type': 'text/plain' });
