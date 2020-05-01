8964fffd9a607d2c118df95496b714141f0c60e8
Tanisha L Davey <tanisha.davey2@gmail.com>
2020-04-30 19:07:25 -0600

    Define getAllMessages

--- server.js
+++ server.js
@@ -8,14 +8,36 @@ let messages = [
     { 'id': 3, 'user': 'lorem ipsum', 'message': 'dolor set amet' }
 ]
 
+const getAllMessages = response => {
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    response.write(JSON.stringify(messages));
+    response.end();
+}
+
+
+
 server.listen(3000, () => {
     console.log('The HTTP server is listening on Port 3000.');     
 })
 
+server.on('request', (request, response) => {
+    if (request.method === 'GET') {
+      getAllMessages(response);
+    }
+  
+    else if (request.method === 'POST') {
+      let newMessage = { 'id': new Date() };
+  
+      request.on('data', (data) => {
+        newMessage = Object.assign(newMessage, JSON.parse(data));
+      });
+  
+      request.on('end', () => {
+        addMessage(newMessage, response);
+      });
+    }
+  });
+
  
 
-server.on('request', (request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.write('Hello World\n');
-    response.end();
-});
+  
